8ef22b5ad704d2bf7f2286841c68e87a3632815a
Arun S. Negi <arunsnegi@protonmail.com>
2020-06-29 21:28:45 +0530

    Fix Grammar and update email

--- about.sh
+++ about.sh
@@ -9,8 +9,8 @@ reset="$(tput sgr0)"
 echo "
 ${bold}${green}    ___ ${reset}
 ${bold}${green}   /   |${reset}   Hi. I'm ${green}${bold}Arun Negi${reset}. I am a student and free software enthusiast.
-${bold}${green}  / /| |${reset}   I love computers and tinkering with it.
-${bold}${green} / ___ |${reset}   Contact: ${green}${bold}arunsnegi@protonmail.com${reset}
+${bold}${green}  / /| |${reset}   I love computers and tinkering with them.
+${bold}${green} / ___ |${reset}   Contact: ${green}${bold}arunsnegi@pm.me${reset}
 ${bold}${green}/_/  |_|${reset}   Github: ${green}${bold}@notarun${reset}
 
 "
